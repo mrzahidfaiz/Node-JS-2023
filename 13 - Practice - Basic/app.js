82679de877f82a89337e419e27e6d6c2baa9f90e
Muhammad Zahid faiz <mrzahidfaiz@gmail.com>
2023-05-07 22:13:32 +0500

    apply condition on paths by req.url

--- 13 - Practice - Basic/app.js
+++ 13 - Practice - Basic/app.js
@@ -1,13 +1,17 @@
 // console.log("Hello World!");
 
-const http = require('http');
+const http = require("http");
 
 const PORT = 3000;
 
-const myServer = http.createServer((req, res) =>{
+const myServer = http.createServer((req, res) => {
+  if (req.url === "/") {
     res.write("Home Page");
     res.end();
+  } else {
+    res.write("404 - Not Found");
+    res.end();
+  }
 });
-
 
 myServer.listen(PORT, console.log("Server Connected!"));
