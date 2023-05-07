80ac64e264b9dd182be8e109c9bb409c3023298d
Muhammad Zahid faiz <mrzahidfaiz@gmail.com>
2023-05-07 22:21:58 +0500

    render html element

--- 13 - Practice - Basic/app.js
+++ 13 - Practice - Basic/app.js
@@ -8,6 +8,10 @@ const myServer = http.createServer((req, res) => {
   if (req.url === "/") {
     res.write("Home Page");
     res.end();
+  } else if (req.url === "/form"){
+    res.setHeader("Content-Type", "text/html");
+    res.write("<form> <input /><button>Submit</button></form>");
+    res.end();
   } else {
     res.write("404 - Not Found");
     res.end();
