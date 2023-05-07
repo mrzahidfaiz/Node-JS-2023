164b99132ff7636490a6225c3655a0cc54061f0b
Muhammad Zahid faiz <mrzahidfaiz@gmail.com>
2023-05-07 22:49:10 +0500

    recieved data form form & show in console

--- 13 - Practice - Basic/app.js
+++ 13 - Practice - Basic/app.js
@@ -11,9 +11,15 @@ const myServer = http.createServer((req, res) => {
   } else if (req.url === "/form"){
     res.setHeader("Content-Type", "text/html");
     // by default its send Get request so we use method in form tag; by default button action as sumbit
-    res.write("<form action='/submit' method='POST'><input name='Data' /><button>Submit</button></form>");
+    res.write("<form action='/submit' method='POST'><input name='data' /><button>Submit</button></form>");
     res.end();
   } else if (req.url === "/submit" ) {
+    // Now Just Take data form the input filed an show in console 
+    // data always recieved in chunk and here, [on] is a eventListner
+    // so now putt data as empty sting; let data = ""; merge data in chunk [chunk => data+=chunk]
+    let data = "";
+    req.on("data", chunk => data+=chunk); // aftr recieving complete data next is
+    req.on("end" , () => console.log(data)); // its show Result in console
     res.write("Data Submited Success");
     res.end();
   } else {
